0e96e25c2498b5ce26b67bdd2f43252b8ba3214f
Haris Kovac <hariskovac19@gmail.com>
2023-02-12 16:32:15 -0600

    feat: Create "operate" function
    The operate function takes an operator and two
    numbers and calls one of the four basic calculator
    functions depending on the operator

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y;
 }
+
+function operate(operator, x, y) {
+  switch (operator) {
+    case "+":
+      add(x, y);
+      break;
+    
+    case "-":
+      subtract(x, y);
+      break;
+
+    case "*":
+      multiply(x, y);
+      break;
+
+    case "/":
+      divide(x, y);
+      break;
+      
+    default:
+      break;
+  }
+}
