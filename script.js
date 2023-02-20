6e02eefc5421cee6170052ab0cbb772dffa4ab07
Haris Kovac <hariskovac19@gmail.com>
2023-02-19 18:24:13 -0600

    refactor: updateScreen function
    Change the updateScreen function to update num1,
    num2, and operator variables and to call the operate
    function if user selects another operator after
    already entering both numbers.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const screen = document.querySelector(".screen");
 const digits = document.querySelectorAll(".digit"); 
 const operators = document.querySelectorAll(".operator");
 
-let num1;
-let num2;
-let operator;
+let num1 = "";
+let num2 = "";
+let operator = "";
 
 digits.forEach((digit) => {
   digit.addEventListener("click", updateScreen); 
@@ -15,54 +15,62 @@ operators.forEach((operator) => {
 })
 
 function updateScreen(e) {
-  if (e.target.classList.contains("operator")) {
-    if (screen.textContent == 0) {
-      screen.textContent = 0 + e.target.textContent;
-    } else if (!screen.textContent.match(/\D/g)) {
-      screen.textContent += e.target.textContent;
-    }
-  }
-  
   if (e.target.classList.contains("digit")) {
-    if (screen.textContent == 0) {
-      screen.textContent = e.target.textContent;
+    if (operator === "") {
+      num1 += e.target.textContent;
     } else {
-      screen.textContent += e.target.textContent;
+      num2 += e.target.textContent;  
     }
   }
+
+  if (e.target.classList.contains("operator")) {
+    if (num1 === "") {
+      num1 = 0;
+    } else if (num2 === "") {
+      operator = e.target.textContent;
+    } else {
+      operate(operator, parseInt(num1), parseInt(num2));
+    }
+  }
+
+  screen.textContent = num1 + operator + num2;
 }
 
 function add(x, y) {
+  console.log(x + y);
   return x + y;
 }
 
 function subtract(x, y) {
+  console.log(x - y);
   return x - y;
 }
 
 function multiply(x, y) {
+  console.log(x * y);
   return x * y;
 }
 
 function divide(x, y) {
+  console.log(x / y);
   return x / y;
 }
 
 function operate(operator, x, y) {
   switch (operator) {
-    case "+":
+    case " + ":
       add(x, y);
       break;
     
-    case "-":
+    case " - ":
       subtract(x, y);
       break;
 
-    case "*":
+    case " \u00D7 ":
       multiply(x, y);
       break;
 
-    case "/":
+    case " / ":
       divide(x, y);
       break;
       
